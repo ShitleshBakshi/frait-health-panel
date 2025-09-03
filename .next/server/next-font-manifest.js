cb5627e61dd2b0b202b2ce7b2c401ce2a94e2f2d
shitlesh <shitlesh.bakshi@gmail.com>
2025-09-03 13:05:43 +0100

    Pushing local frontend changes

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/shitleshbakshi/FRAIT/WebstormProjects/Efrait_Stage_Frontend/frontend/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
